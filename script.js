2be755909b54e9afea49cbbaad64b45430db7cff
juan david marin <juaanmarin2007@gmail.com>
2022-04-08 11:37:12 -0500

    se realizo un cambio al disenio de las interfaces y se agrego la funcionalidad para ver la fecha

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const $tiempo=document.querySelector(".tiempo"),
-$fecha = document.querySelector(".fecha");
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
 
-function digitalclock(){
-    let hora =new Date(),
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
 
-    dia= hora.getDate(),
-    mes = hora.getMonth(),
-    anio = hora.getSeconds(),
-    diasemana = hora.getDay();
-
-    let timeString=hora.toLocaleTimeString();
-    $tiempo.innerHTML=timeString;
-
-    $fecha.innerHTML="2021-11-12"
-   
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+    
+    var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var days = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'];
+    var curWeekDay = days[today.getDay()];
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var curDay = today.getDate();
+    var date =`${curWeekDay}, ${curDay} ${curMonth} ${curYear}`;
+    document.getElementById("date").innerHTML = date;
+    
 }
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
 setInterval(() =>{
-    digitalclock()
+    startTime()
 },1000);
 
-
-
-    
-    
